0fe405ae8dd2c7716a23e4d70f0b9e056a459077
Cornelis Vletter <cvletter@1bite.nl>
2020-10-03 10:49:46 +0200

    file management

---
+++ prediction/file_management.py
@@ -0,0 +1,13 @@
+# File locations
+RAW_DATA = 'U:\\Data\Betellingen met HF-artikel.xlsx'
+PRODUCT_STATUS = 'U:\\Data\productstatus.xlsx'
+WEER_DATA = 'U:\\Data\knmi_200913_debilt.csv'
+SAVE_LOC = 'U:\\Data\Prepared'
+
+# PREPARED DATA
+WEER_DATA_PREP = 'weer_data_processed_2020926_1610.csv'
+ORDER_DATA_ACT = 'actieve_halffabricaten_wk_2020926_1610.csv'
+ORDER_DATA_INACT = 'inactieve_halffabricaten_wk_2020926_1610.csv'
+EXOG_FEATURES = 'exogenous_features_2020926_1620.csv'
+FIT_DATA = 'fit_data_2020926_1658.p'
+PREDICT_DATA = 'prediction_data_2020926_1658.p'
