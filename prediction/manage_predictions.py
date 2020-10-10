d5eb438307a4dad24e7c7e7262c48da2a97592a4
Cornelis Vletter <cvletter@1bite.nl>
2020-10-10 17:16:31 +0200

    Updated evaluations

--- prediction/manage_predictions.py
+++ prediction/manage_predictions.py
@@ -27,39 +27,9 @@ inactive_products_act = gf.import_temp_file(file_name=fm.ORDER_DATA_INACT, data_
 all_products_act = active_products_act.join(inactive_products_act, how='outer')
 
 eval_nb1 = eval_pred.prediction_performance_evaluation(Y_true=all_products_act, Y_pred=pred_nb1,
-                                                        Y_pred_mod=mod_prod_nb1, Y_pred_non_mod=non_mod_prod_nb1)
+                                                       Y_pred_mod=mod_prod_nb1, Y_pred_non_mod=non_mod_prod_nb1)
 
 eval_nb2 = eval_pred.prediction_performance_evaluation(Y_true=all_products_act, Y_pred=pred_nb2,
-                                                        Y_pred_mod=mod_prod_nb2, Y_pred_non_mod=non_mod_prod_nb2)
+                                                       Y_pred_mod=mod_prod_nb2, Y_pred_non_mod=non_mod_prod_nb2)
 
-pct_fits = is_pct_nb2
-
-def in_sample_evaluation(pct_fits):
-    eval_time = pd.DataFrame(index=pct_fits.index)
-    eval_time['average'] = pct_fits.mean(axis=1)
-    eval_time['5p'] = pct_fits.quantile(q=0.05, axis=1)
-    eval_time['25p'] = pct_fits.quantile(q=0.25, axis=1)
-    eval_time['median'] = pct_fits.quantile(q=0.5, axis=1)
-    eval_time['75p'] = pct_fits.quantile(q=0.75, axis=1)
-    eval_time['95p'] = pct_fits.quantile(q=0.95, axis=1)
-    eval_time['count of products'] = pct_fits.count(axis=1, numeric_only=True)
-
-    print("Overall median: {}; overall 5p: {}; overall 95p: {}".format(round(eval_time['median'].median(), 3),
-                                                                       round(eval_time['5p'].median(), 3),
-                                                                       round(eval_time['95p'].median(), 3)))
-
-    eval_prod = pd.DataFrame(index=pct_fits.columns)
-    eval_prod['average'] = pct_fits.mean(axis=0)
-    eval_prod['5p'] = pct_fits.quantile(q=0.05, axis=0)
-    eval_prod['25p'] = pct_fits.quantile(q=0.25, axis=0)
-    eval_prod['median'] = pct_fits.quantile(q=0.5, axis=0)
-    eval_prod['75p'] = pct_fits.quantile(q=0.75, axis=0)
-    eval_prod['95p'] = pct_fits.quantile(q=0.95, axis=0)
-    eval_time['count of fits'] = pct_fits.count(axis=0, numeric_only=True)
-
-    print("Overall median: {}; overall 5p: {}; overall 95p: {}".format(round(eval_prod['median'].median(), 3),
-                                                                       round(eval_prod['5p'].median(), 3),
-                                                                       round(eval_prod['95p'].median(), 3)))
-
-
-sns.relplot(data=is_pct_nb2[cn.MOD_PROD_SUM], kind="line")
+eval_is_nb1_time, eval_is_nb1_prod = eval_pred.in_sample_evaluation(pct_fits=is_pct_nb1)
