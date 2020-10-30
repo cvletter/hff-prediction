a61a9db8d094c604efd1a7f290b1c6b38a1d0aeb
Cornelis Vletter <cvletter@1bite.nl>
2020-10-30 20:48:58 +0100

    Working version of bootstrap parallel processing

---
+++ bootstrap_batch_prediction.py
@@ -0,0 +1,57 @@
+import prediction.make_prediction as pred
+import prediction.prediction_evaluation as eval_pred
+import prediction.column_names as cn
+import prediction.file_management as fm
+import prediction.general_purpose_functions as gf
+import pandas as pd
+import multiprocessing
+import time
+
+# Prediction
+model_settings = {'prediction_window': 1, 'train_size': 60, 'differencing': False, 'ar_lags': 4,
+                 'fit_model': 'OLS', 'feature_threshold': [0.2, 15]}
+
+
+def batch_prediction(prediction_date):
+
+    print("Starting for date: {}".format(prediction_date))
+
+    p_window = model_settings['prediction_window']
+    train_size = model_settings['train_size']
+    differencing = model_settings['differencing']
+    ar_lags = model_settings['ar_lags']
+    fit_model = model_settings['fit_model']
+    feature_threshold = model_settings['feature_threshold']
+
+    _predict = pred.run_prediction_bootstrap(
+        date_to_predict=prediction_date, prediction_window=p_window, train_obs=train_size,
+        difference=differencing, lags=ar_lags, order_data=fm.RAW_DATA, weather_data=fm.WEER_DATA,
+        product_data=fm.PRODUCT_STATUS, model_type=fit_model, feature_threshold=[feature_threshold[0],
+                                                                                 feature_threshold[1]])
+
+    return _predict
+
+
+if __name__ == "__main__":
+    start = time.time()
+    prediction_dates = pd.DataFrame(pd.date_range(end='2020-10-5', periods=10, freq='W-MON').astype(str),
+                                    columns=[cn.FIRST_DOW])
+
+    pred_dates = list(prediction_dates[cn.FIRST_DOW])
+
+    num_cores = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(num_cores)
+
+    results = pool.map(batch_prediction, pred_dates)
+    pool.close()
+    pool.join()
+
+    all_preds = pd.concat(results)
+
+    print(all_preds)
+
+    elapsed = round((time.time() - start), 2)
+    print("It takes {} seconds to run a prediction.".format(elapsed))
+
+
+
--- prediction/make_prediction.py
+++ prediction/make_prediction.py
@@ -8,6 +8,83 @@ import prediction.column_names as cn
 import prediction.general_purpose_functions as gf
 import pandas as pd
 import time
+import numpy as np
+
+
+def run_prediction_bootstrap(date_to_predict=cn.PREDICTION_DATE, prediction_window=cn.PREDICTION_WINDOW, train_obs=cn.TRAIN_OBS,
+                             difference=False, lags=cn.N_LAGS, order_data=fm.RAW_DATA, weather_data=fm.WEER_DATA,
+                             product_data=fm.PRODUCT_STATUS, model_type='OLS', feature_threshold=None):
+    if feature_threshold is None:
+        feature_threshold = [0.2, 15]
+
+    def convert_series_to_dataframe(input_series, date_val, index_name=cn.FIRST_DOW):
+        input_df = pd.DataFrame(input_series).T
+        input_df[index_name] = date_val
+        return input_df.set_index(index_name, drop=True, inplace=False)
+
+    def in_sample_error(all_fits, all_true_values):
+        fit_error = abs(all_fits.subtract(all_true_values[all_fits.columns], axis='index'))
+        avg_fit_error = fit_error.mean(axis=0)
+        corr_true_values = all_true_values.replace(0, 1)
+        pct_fit_error = fit_error / corr_true_values
+        avg_pct_fit_error = pct_fit_error.mean(axis=0)
+
+        avg_fit_error_df = convert_series_to_dataframe(input_series=avg_fit_error, date_val=date_to_predict)
+        avg_pct_fit_error_df = convert_series_to_dataframe(input_series=avg_pct_fit_error, date_val=date_to_predict)
+
+        return avg_fit_error_df, avg_pct_fit_error_df
+
+    # Import and prepare data
+    active_products, inactive_products, weather_data_processed, order_data_su = data_prep_wrapper(
+        prediction_date=date_to_predict,
+        prediction_window=prediction_window,
+        reload_data=False,
+        order_data_loc=order_data,
+        weer_data_loc=weather_data,
+        product_data_loc=product_data,
+        agg_weekly=True, exclude_su=True,
+        save_to_csv=False)
+
+    exogenous_features = prep_all_features(weather_data_processed=weather_data_processed,
+                                           order_data_su=order_data_su,
+                                           prediction_date=date_to_predict,
+                                           train_obs=train_obs,
+                                           save_to_csv=False)
+
+    fit_data, predict_data = prediction_setup_wrapper(
+        prediction_date=date_to_predict,
+        prediction_window=prediction_window,
+        train_obs=train_obs,
+        nlags=lags,
+        difference=difference,
+        act_products=active_products,
+        exog_features=exogenous_features,
+        save_to_pkl=False)
+
+    __, all_predictions = fit_and_predict(
+        fit_dict=fit_data, predict_dict=predict_data,
+        model_type=model_type,
+        feature_threshold=[feature_threshold[0],
+                           feature_threshold[1]])
+
+    all_predictions['iteration'] = 0
+
+    for i in range(1, 40):
+        print("Running iterations {}".format(i))
+        __, temp_os = fit_and_predict(fit_dict=fit_data, predict_dict=predict_data, bootstrap=True,
+                                      model_type=model_type, feature_threshold=[feature_threshold[0],
+                                                                                feature_threshold[1]])
+        temp_os['iteration'] = i
+
+        all_predictions = pd.concat([all_predictions, temp_os])
+
+
+
+
+    # fit_data['avg_fit_error'], fit_data['avg_pct_fit_error'] = in_sample_error(all_fits=in_sample_fit, all_true_values=fit_data['y_true'])
+    # in_sample_fit, prediction_os, fit_data, predict_data
+
+    return all_predictions
 
 
 def run_prediction(date_to_predict=cn.PREDICTION_DATE, prediction_window=cn.PREDICTION_WINDOW, train_obs=cn.TRAIN_OBS,
@@ -108,14 +185,14 @@ if __name__ == '__main__':
     start = time.time()
     # In sample testing of 2020-31-8
     is_fit1, os_pr1, fit_data1, predict_data1 = run_prediction(date_to_predict='2020-08-10',
-                                                                                   prediction_window=1,
-                                                                                   train_obs=cn.TRAIN_OBS,
-                                                                                   difference=False, lags=4,
-                                                                                   order_data=fm.RAW_DATA,
-                                                                                   weather_data=fm.WEER_DATA,
-                                                                                   product_data=fm.PRODUCT_STATUS,
-                                                                                   model_type='OLS',
-                                                                                   feature_threshold=[0.2, 15])
+                                                                         prediction_window=1,
+                                                                         train_obs=cn.TRAIN_OBS,
+                                                                         difference=False, lags=4,
+                                                                         order_data=fm.RAW_DATA,
+                                                                         weather_data=fm.WEER_DATA,
+                                                                         product_data=fm.PRODUCT_STATUS,
+                                                                         model_type='OLS',
+                                                                         feature_threshold=[0.2, 15])
 
     elapsed = round((time.time() - start), 2)
     print("It takes {} seconds to run a prediction.".format(elapsed))
