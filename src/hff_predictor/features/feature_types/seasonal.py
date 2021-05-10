f7347955195c39b45d148e0dce9d13388aef0624
Cornelis Vletter <cvletter@1bite.nl>
2021-05-10 19:15:20 +0200

    Added holidays

--- src/hff_predictor/features/feature_types/seasonal.py
+++ src/hff_predictor/features/feature_types/seasonal.py
@@ -13,39 +13,92 @@ def prep_holiday_features():
     )
 
     # Christmas
-    pre_christmas_dt = pd.to_datetime(
+    christmas_dt = pd.to_datetime(
         [
-            "2018-12-10",
-            "2018-12-17",
-            "2019-12-09",
-            "2019-12-16",
-            "2020-12-14",
+            "2018-12-24",
+            "2019-12-23",
             "2020-12-21",
-            "2021-12-13",
-            "2021-12-20"
+            "2021-12-20",
+            "2022-12-19"
         ]
     )
-    add_holiday(holiday="pre_christmas", output_df=holiday_dates, dates=pre_christmas_dt)
 
-
-    post_christmas_dt = pd.to_datetime(
-        ["2018-12-24", "2018-12-31", "2019-12-30", "2020-12-28", "2021-01-04", "2021-12-27", "2022-01-03"]
+    newyears_dt = pd.to_datetime(
+        [
+            "2018-12-31",
+            "2019-12-30",
+            "2020-12-28",
+            "2021-12-27",
+            "2022-12-26"
+        ]
     )
-    holiday_dates["post_christmas"] = [
-        1 if x in post_christmas_dt else 0 for x in holiday_dates["day"]
-    ]
 
-    # Pasen, Pinksteren, Goede Vrijdag test
+    easter_dt = pd.to_datetime(
+        [
+            "2018-03-26",
+            "2019-04-15",
+            "2020-04-06",
+            "2021-03-29",
+            "2022-04-11"
+        ]
+    )
 
-    # Koningsdag
+    pentecost_dt = pd.to_datetime(
+        [
+            "2018-05-14",
+            "2019-06-03",
+            "2020-05-25",
+            "2021-05-17",
+            "2022-05-30"
+        ]
+    )
 
-    # Jaarwisseling
+    mothers_dt = pd.to_datetime(
+        [
+            "2018-05-07",
+            "2019-05-06",
+            "2020-05-24",
+            "2021-05-03",
+            "2022-05-02"
+        ]
+    )
+
+    fathers_dt = pd.to_datetime(
+        [
+            "2018-06-11",
+            "2019-06-10",
+            "2020-06-15",
+            "2021-06-14",
+            "2022-06-13"
+        ]
+    )
+
+    sinterklaas_dt = pd.to_datetime(
+        [
+            "2018-11-26",
+            "2019-12-02",
+            "2020-11-30",
+            "2021-11-29",
+            "2022-11-28"
+        ]
+    )
+
+    add_holiday(holiday="christmas", output_df=holiday_dates, dates=christmas_dt)
+    add_holiday(holiday="newyears", output_df=holiday_dates, dates=newyears_dt)
+    add_holiday(holiday="easter", output_df=holiday_dates, dates=easter_dt)
+    add_holiday(holiday="pentecost", output_df=holiday_dates, dates=pentecost_dt)
+    add_holiday(holiday="christmas", output_df=holiday_dates, dates=christmas_dt)
+    add_holiday(holiday="mothers_day", output_df=holiday_dates, dates=mothers_dt)
+    add_holiday(holiday="fathers_day", output_df=holiday_dates, dates=fathers_dt)
+    add_holiday(holiday="sinterklaas", output_df=holiday_dates, dates=sinterklaas_dt)
 
     gf.add_week_year(data=holiday_dates, date_name="day")
     gf.add_first_day_week(
         add_to=holiday_dates, week_col_name=cn.WEEK_NUMBER, set_as_index=True
     )
     holiday_dates.drop("day", axis=1, inplace=True)
+
+    holiday_dates["all_holidays"] = holiday_dates.sum(axis=1)
 
     return holiday_dates.groupby(cn.FIRST_DOW, as_index=True).max()
 
