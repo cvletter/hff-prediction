ee5357b4cd88a35ee3f6b23c533104b3100b9969
Cornelis Vletter <c.vletter@1bite.nl>
2021-06-03 13:04:57 +0200

    Opslaglocaties aangepast tbv Qlik server

--- src/hff_predictor/config/file_management.py
+++ src/hff_predictor/config/file_management.py
@@ -1,30 +1,24 @@
-# File locations
-RAW_DATA = r"U:\Data\Bestellingen per HF-Artikel Update 09-10-2020 11.18.15.xlsx"
-PRODUCT_STATUS = r"U:\Data\productstatus.xlsx"
-SAVE_LOC = r"U:\Data\Prepared"
-WEER_DATA = r"U:\Data\knmi_201007_debilt.csv"
-CAMPAIGN_DATA = r"U:\Data\campagne_overzicht_totaal.csv"
-
 # Key folders
+BASE_FOLDER = "E:\Productie Voorspelmodel\Data"
 
 # Input folders
-ORDER_DATA_FOLDER = r"U:\Productie Voorspelmodel\Input\Bestellingen"
-PRODUCT_STATUS_FOLDER = r"U:\Productie Voorspelmodel\Input\Productstatus"
-CAMPAIGN_DATA_FOLDER = r"U:\Productie Voorspelmodel\Input\Campagnes"
+ORDER_DATA_FOLDER = BASE_FOLDER + "\Input\Bestellingen"
+PRODUCT_STATUS_FOLDER = BASE_FOLDER + "\Input\Productstatus"
+CAMPAIGN_DATA_FOLDER = BASE_FOLDER + "\Input\Campagnes"
 
 # Processed folders
-WEATHER_DATA_PPR_FOLDER = r"U:\Productie Voorspelmodel\Processed\Weer\Preprocessed"
-WEATHER_DATA_PR_FOLDER = r"U:\Productie Voorspelmodel\Processed\Weer\Processed"
-ORDER_DATA_PR_FOLDER = r"U:\Productie Voorspelmodel\Processed\Bestellingen\Standaard"
-ORDER_DATA_SU_PPR_FOLDER = r"U:\Productie Voorspelmodel\Processed\Bestellingen\Superunie eigenschappen\Preprocessed"
-CAMPAIGN_DATA_PR_FOLDER = r"U:\Productie Voorspelmodel\Processed\Campagnes"
+WEATHER_DATA_PPR_FOLDER = BASE_FOLDER + "\Processed\Weer\Preprocessed"
+WEATHER_DATA_PR_FOLDER = BASE_FOLDER + "\Processed\Weer\Processed"
+ORDER_DATA_PR_FOLDER = BASE_FOLDER + "\Processed\Bestellingen\Standaard"
+ORDER_DATA_SU_PPR_FOLDER = BASE_FOLDER + "\Processed\Bestellingen\Superunie eigenschappen\Preprocessed"
+CAMPAIGN_DATA_PR_FOLDER = BASE_FOLDER + "\Processed\Campagnes"
 
-ORDER_DATA_ACT_PR_FOLDER = r"U:\Productie Voorspelmodel\Processed\Bestellingen\Actief"
-ORDER_DATA_INACT_PR_FOLDER = r"U:\Productie Voorspelmodel\Processed\Bestellingen\Inactief"
-ORDER_DATA_SU_PR_FOLDER = r"U:\Productie Voorspelmodel\Processed\Bestellingen\Superunie eigenschappen\Processed"
+ORDER_DATA_ACT_PR_FOLDER = BASE_FOLDER + "\Processed\Bestellingen\Actief"
+ORDER_DATA_INACT_PR_FOLDER = BASE_FOLDER + "\Processed\Bestellingen\Inactief"
+ORDER_DATA_SU_PR_FOLDER = BASE_FOLDER + "\Processed\Bestellingen\Superunie eigenschappen\Processed"
 
-ORDER_DATA_CG_PR_FOLDER = r"U:\Productie Voorspelmodel\Processed\Bestellingen\Consumentgroepnummer"
-FEATURES_PROCESSED_FOLDER = "U:\Productie Voorspelmodel\Processed\Features"
+ORDER_DATA_CG_PR_FOLDER = BASE_FOLDER + "\Processed\Bestellingen\Consumentgroepnummer"
+FEATURES_PROCESSED_FOLDER = BASE_FOLDER + "\Processed\Features"
 
 
 # Processed files
@@ -40,24 +34,10 @@ PRODUCT_CONSUMENTGROEP_NR = "product_consumentgroep_nr"
 FEATURES_PROCESSED = "exogenous_features_processed"
 
 # Output
-PREDICTIONS_FOLDER = r"U:\Productie Voorspelmodel\Output\Voorspellingen"
-INTERMEDIARY_RESULTS_FOLDER = r"U:\Productie Voorspelmodel\Output\Tussenresultaten"
-TEST_RESULTS_FOLDER = r"U:\Productie Voorspelmodel\Output\Testvoorspellingen"
-TEST_PREDICTIONS_FOLDER = r"U:\Productie Voorspelmodel\Output\Tussenresultaten\Performance"
-
-# Intermediary results
-ORDER_DATA_PIVOT_WK = "order_data_pivot_wk_proc_20201031_856.csv"
-CAMPAIGN_DATA_PROC = "campaign_data_processed_20201114_1222.csv"
-WEER_DATA_PRE_PROC = "weather_data_pre_processed_20201031_856.csv"
-ORDER_DATA_WK_SU = "order_data_wk_su_proc_20201031_856.csv"
-WEER_DATA_PREP = "weer_data_processed_20201031_93.csv"
-ORDER_DATA_ACT = "actieve_halffabricaten_week_2021427_1126.csv"
-ORDER_DATA_INACT = "inactieve_halffabricaten_week_2021427_1126.csv"
-ORDER_DATA_ACT_SU = "actieve_halffabricaten_wk_su_20201031_93.csv"
-EXOG_FEATURES = "exogenous_features_20201112_1645.csv"
-FIT_DATA = "fit_data_20201112_172.p"
-PREDICT_DATA = "predict_data_20201112_172.p"
-
+PREDICTIONS_FOLDER = BASE_FOLDER + "\Output\Voorspellingen"
+INTERMEDIARY_RESULTS_FOLDER = BASE_FOLDER + "\Output\Tussenresultaten"
+TEST_RESULTS_FOLDER = BASE_FOLDER + "\Output\Testvoorspellingen"
+TEST_PREDICTIONS_FOLDER = BASE_FOLDER + "\Output\Tussenresultaten\Performance"
 
 # Feature groups
 WEATHER = "weather"
